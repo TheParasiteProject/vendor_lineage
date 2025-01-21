557316e26bf5b33e956f60595375a8dcc1007e71
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-01-21 17:32:37 +0900

    vendor: Add sdk build version for display
    Change-Id: I86cff2c72167b36f48957f656337341d312a7386

--- config/version.mk
+++ config/version.mk
@@ -42,4 +42,5 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.custom.version=$(LINEAGE_VERSION) \
     ro.custom.display.version=$(LINEAGE_DISPLAY_VERSION) \
     ro.custom.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.custom.sdk.build.version?=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
     ro.custom.releasetype=$(LINEAGE_BUILDTYPE)
